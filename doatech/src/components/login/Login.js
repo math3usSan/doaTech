768222d7b0cb8b6e22f77862d7d10cc7b75e25dd
Matheus Santos <111880593+math3usSan@users.noreply.github.com>
2024-10-01 21:39:25 -0300

    first commit

---
+++ doatech/src/components/login/Login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {FaUser, FaLock} from 'react-icons/fa';
+
+import { useState } from 'react';
+
+import "./Login.css";
+
+const Login = () => {
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("teste", userName, password);
+
+        console.log("Envio");
+    };
+
+  return (
+    <div className='container'>
+        <form onSubmit={handleSubmit}>
+            <h1>Acesse o sistema</h1>
+            <div>
+                <input type='email' placeholder='E-mail' onChange={(e) => setUserName(e.target.value)}></input>
+                <FaUser className='icon' />
+            </div>
+            <div>
+                <input type='password' placeholder='Senha'></input>
+                <FaLock className='icon' />
+            </div>
+
+            <div className='recall-forget'>
+                <label type="checkbox">
+                    Lembre de mim
+                </label>
+                <a href='#'>Esqueceu a senha</a>
+            </div>
+
+            <button>Entrar</button>
+
+            <div className='signup-link'>
+                <p>Não tem uma conta? <a href='#'>Registrar</a></p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
